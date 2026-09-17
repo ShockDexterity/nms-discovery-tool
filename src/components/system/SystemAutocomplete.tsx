74756201cb8881f51513c0efb6a45fda5f1439e7
shockdexterity <sableeye13@gmail.com>
2026-09-17 00:32:46 -0500

    sort systems by _id which puts most recent at top

--- src/components/system/SystemAutocomplete.tsx
+++ src/components/system/SystemAutocomplete.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-import { TextField } from "@mui/material";
-
 import MyAutocomplete from "@/components/general/MyAutocomplete";
 
 import { System } from "@/lib/types";
-import { ErrorBoundary } from "react-error-boundary";
 
 type Props = Readonly<{ system_list_promise: Promise<System[]>; defaultValue?: string }>;
 
@@ -15,8 +12,8 @@ export default function SystemAutocomplete({ system_list_promise, defaultValue =
       label="System Name"
       name="system"
       options={React.use(system_list_promise)
-        .map((s) => s.name)
-        .sort((a, b) => a.localeCompare(b))}
+        .sort((a, b) => b._id.localeCompare(a._id))
+        .map((s) => s.name)}
       defaultValue={defaultValue}
     />
   );
